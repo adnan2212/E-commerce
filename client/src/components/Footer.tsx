26fa88727a055a800c45c1946caa7237983fad7e
adnan2212 <adnanshaikh2607@gmail.com>
2025-10-12 00:18:38 +0530

    feat: add footer UI component

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,6 +1,44 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <div className="">Footer</div>
+    <div className="mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start md:justify-between md:gap-0 bg-gray-800 p-8 rounded-lg">
+      <div className="flex flex-col gap-4 items-center md:items-start">
+        <Link href="/" className="flex items-center">
+        <Image 
+          src={"/logo.png"}
+          alt="webbuy" 
+          width={36} 
+          height={36} 
+        />
+        <p className="hidden md:block text-md font-medium tracking-wider text-white">WEB BUY</p>
+        </Link >
+        <p className="text-sm text-gray-400">&copy; 2025 WEB BUY</p>
+        <p className="text-sm text-gray-400">All rights reserved.</p>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href="/">All Products</Link>
+        <Link href="/">New Arrivals</Link>
+        <Link href="/">Best Sellers</Link>
+        <Link href="/">Sale</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Company</p>
+        <Link href="/">About</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Blog</Link>
+        <Link href="/">Affilate Program</Link>
+      </div>
+    </div>
   )
 }
 
