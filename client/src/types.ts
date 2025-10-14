06906fddbee14e8684ae67d159da059b0bee1a2c
adnan2212 <adnanshaikh2607@gmail.com>
2025-10-14 21:14:27 +0530

    feat: integrated Zustand for state management of cart items, persisted cart data in local storage, add checkout page

--- client/src/types.ts
+++ client/src/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export type ProductType = {
   id: string | number;
   name: string;
@@ -10,3 +12,50 @@ export type ProductType = {
 }
 
 export type ProductsType = ProductType[]
+
+export type CartItemType = ProductType & {
+    quantity: number,
+    selectedSize: string,
+    selectedColor: string
+};
+
+export type CartItemsType = CartItemType[]
+
+export const shippingFormSchema = z.object({
+  name: z.string().min(1, "Name is required!"),
+  email: z.string().min(1, "Email is required!"),
+  phone: z
+    .string()
+    .regex(/^\d{10}$/, "Phone number must be exactly 10 digits!"),
+  address: z.string().min(1, "Address is required!"),
+  city: z.string().min(1, "City is required!"),
+});
+
+export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+
+export const paymentFormSchema = z.object({
+  cardHolder: z.string().min(1, "Card holder is required!"),
+  cardNumber: z.string().min(16, "CardNumber is required!").max(16, "CardNumber is required!"),
+  expirationDate: z
+    .string()
+    .regex(
+      /^(0[1-9]|1[0-2])\/\d{2}$/,
+      "Expiration date must be in MM/YY format"
+    ),
+  cvv: z
+    .string()
+    .regex(/^\d{3}$/, "CVV must be exactly 3 digits"),
+});
+
+export type PaymentFormInputs = z.infer<typeof paymentFormSchema>;
+
+export type CartStoreStateType = {
+  cart: CartItemsType;
+  hasHydrated: boolean;
+}
+
+export type CartStoreActionsType = {
+  addToCart: (product: CartItemType) => void;
+  removeFromCart: (product: CartItemType) => void;
+  clearCart: () => void;
+}
